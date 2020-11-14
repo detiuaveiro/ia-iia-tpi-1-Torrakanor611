ec6bf4f1dea16336cd4446a259c03bf64fed56ce
Pedro <pedro.abreu@ua.pt>
2020-11-14 23:13:51 +0000

    last version

--- tpi1.py
+++ tpi1.py
@@ -7,13 +7,15 @@ from strips import *
 import sys
 import operator
 
-
 class MyTree(SearchTree):
-
     def __init__(self,problem, strategy='breadth'): 
         super().__init__(problem,strategy)
-        # self.from_init = MyTree(self.problem)
-        # self.to_goal = MyTree(self.problem)
+        # redifine root to be Aux Instance
+        self.root = Aux(problem.initial, None)
+        # redifine self.open_ones
+        self.open_nodes = [self.root]
+        self.from_init = None
+        self.to_goal = None
 
     def hybrid1_add_to_open(self,lnewnodes):
         for i in range(len(lnewnodes)):
@@ -24,17 +26,12 @@ class MyTree(SearchTree):
     
     def hybrid2_add_to_open(self,lnewnodes):
         self.open_nodes.extend(lnewnodes)
-        self.open_nodes = sorted(self.open_nodes, key = lambda x : x.depth - x.offset)
-        # if len(self.open_nodes) == 0:
-        #     if len(lnewnodes) != 0:
-        #         self.open_nodes.append(lnewnodes.pop(0))
-        # for newnode in lnewnodes:
-        #     for node in self.open_nodes:
-        #         if get_num(newnode) > get_num(node):
-        #             self.open_nodes.insert(i, newnode)
-
+        self.open_nodes = sorted(self.open_nodes, key = lambda x : (x.depth - x.offset))
 
     def search2(self):
+        # clear rel
+        # rel is a dict wich contains the last offset mapped to that depth
+        rel = {}
         while self.open_nodes != []:
             node = self.open_nodes.pop(0)
             if self.problem.goal_test(node.state):
@@ -48,33 +45,22 @@ class MyTree(SearchTree):
                 if newstate not in self.get_path(node):
                     newnode = Aux(newstate,node)
                     newnode.depth = get_depth(newnode)
-                    # offset = 0
-                    # for x in self.root.children:
-                    #     newnode.offset += get_num(x, node, offset)
-                    #     offset = 0
+                    if node.depth not in rel.keys():
+                        rel[node.depth] = 0
+                    else:
+                        rel[node.depth] += 1
+                    newnode.offset = rel[node.depth]
                     node.children.append(newnode)
-                    newnode.offset = node.children.index(newnode)
             self.add_to_open(node.children)
-        print("sou none type!")
         return None
 
     def search_from_middle(self):
-        return self.do_search(self.problem.initial, self.problem.goal)
+        m = self.problem.domain.middle(self.problem.initial, self.problem.goal)
 
-    def do_search(self, city1, city2):
-        m = self.problem.domain.middle(city1, city2)
+        self.from_init = MyTree(SearchProblem(self.problem.domain, self.problem.initial, m))
+        self.to_goal = MyTree(SearchProblem(self.problem.domain, m, self.problem.goal))
 
-        # print("city1: " + city1)
-        # print("city2: " + city2)
-        # print("middle: " + str(m))
-
-        if self.problem.domain.result(city1, (city1, city2)) != None:
-            return city1
-
-        path1 += self.do_search(city1, m)
-        path2 += self.do_search(m, city2)
-
-        return path1 + path2
+        return list(dict.fromkeys(self.from_init.search() + self.to_goal.search()))
 
 class Aux(SearchNode):
     def __init__(self,state,parent):
@@ -82,20 +68,55 @@ class Aux(SearchNode):
         self.depth = 0
         self.offset = 0
 
-
 class MinhasCidades(Cidades):
 
     # state that minimizes heuristic(state1,middle)+heuristic(middle,state2)
     def middle(self,city1,city2):
-
-        # print([min(list(map(lambda x : float(self.heuristic(city1, x[0]) + self.heuristic(x[0], city2) - float(self.heuristic(city1, city2))), self.connections)))])
         # 1 linha
         return [con[0] for con in self.connections if float(self.heuristic(city1, con[0])) + float(self.heuristic(con[0], city2)) in [min(list(map(lambda x : float(self.heuristic(city1, x[0]) + self.heuristic(x[0], city2)) if x[0] not in [city1, city2] else sys.maxsize, self.connections)))]][0]
-    
-    # aux = [connection for connection in [min(list(map(lambda x : float(self.heuristic(city1, x[0]) + self.heuristic(x[0], city2)), self.connections)))]) if self.heuristic(city1, x[0]) + self.heuristic(x[0], city2)]
 
+class MySTRIPS(STRIPS):
+    def result(self, state, action):
+        return [c for c in state if c not in action.neg] + action.pos
 
-    # # state that minimizes heuristic(state1,middle)+heuristic(middle,state2)
+    def sort(self,state):
+        return sorted(list(map(lambda x : str(x), state)))
+
+def get_depth(item):
+    if item.parent == None:
+        return 0
+    return 1 + get_depth(item.parent)
+
+# def order_actions(actions, city):
+#     new_actions = []
+#     for act in actions:
+#         if act[1] == city:
+#             new_actions.append((act[1], act[0]))
+#             continue
+#         new_actions.append(act)
+#     return new_actions
+
+# def get_num(node):
+#     if node.parent == None:
+#         return 0
+
+#     depth = 1 + get_num(node.parent)
+
+#     print("node " + str(node))
+#     print(depth)
+#     print(node.parent.children)
+
+#     return depth - ( node.parent.children.index(node) if len(node.parent.children) != None else 0)        
+
+# def get_num(node, newnode_parent, offset):
+#     if node.depth < newnode_parent.depth:
+#         for nodex in node.children:
+#             return get_num(nodex, newnode, offset)
+#     elif node == newnode_parent.depth:
+#         return offset + len(node.children)
+
+# tentativa ex.2:
+# # state that minimizes heuristic(state1,middle)+heuristic(middle,state2)
     # def middle(self,city1,city2):
     #     min = (city1, 10000)
     #     # actions todos as actions possíveis
@@ -133,46 +154,19 @@ class MinhasCidades(Cidades):
     #             new_actions.append(act)
     #     return new_actions
 
+# tentativa ex.3:
+    # def do_search(self, city1, city2):
+    #     return SearchProblem(self.problem.domain, city1, city2)
+    #     m = self.problem.domain.middle(city1, city2)
 
+    #     # print("city1: " + city1)*
+    #     # print("city2: " + city2)
+    #     # print("middle: " + str(m))
 
-class MySTRIPS(STRIPS):
-    def result(self, state, action):
-        return [c for c in state if c not in action.neg] + action.pos
+    #     if self.problem.domain.result(city1, (city1, city2)) != None:
+    #         return city1
 
-    def sort(self,state):
-        return sorted(list(map(lambda x : str(x), state)))
+    #     s
+    #     self.to_goal += self.do_search(m, city2)
 
-# def order_actions(actions, city):
-#     new_actions = []
-#     for act in actions:
-#         if act[1] == city:
-#             new_actions.append((act[1], act[0]))
-#             continue
-#         new_actions.append(act)
-#     return new_actions
-
-def get_depth(item):
-    if item.parent == None:
-        return 0
-    return 1 + get_depth(item.parent)
-
-def get_num(node):
-    if node.parent == None:
-        return 0
-
-    depth = 1 + get_num(node.parent)
-
-    print("node " + str(node))
-    print(depth)
-    print(node.parent.children)
-
-    return depth - ( node.parent.children.index(node) if len(node.parent.children) != None else 0)
-            
-
-def get_num(node, newnode_parent, offset):
-    if node.depth < newnode_parent.depth:
-        for nodex in node.children:
-            return get_num(nodex, newnode, offset)
-    elif node == newnode_parent.depth:
-        return offset + len(node.children)
-
+    #     return  + path2
